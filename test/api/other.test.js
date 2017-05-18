e32683c18b33236a85258872205116cbf125f3f0
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2017-05-18 19:05:14 +0300

    test - api/other

--- test/api/other.test.js
+++ test/api/other.test.js
@@ -1,18 +1,18 @@
 /* global describe, it */
 'use strict';
 
+const request = require('request');
 const {assert} = require('chai');
-
-function summ(arg1, arg2) {
-    return arg1 + arg2;
-}
+const constant = require('./../const.json');
+const apiRoute = require('./../../http/api-route.json');
 
 describe('api/other', () => {
-    it('test test', () => {
-        assert.equal(summ(2, 2), 4);
-    });
-
-    it('test test', () => {
-        assert.equal(summ(2, 2), 4);
+    it('get public id', done => {
+        request(
+            {url: constant.url + apiRoute.route.other.getPublicId.replace(':privateUserId', constant.privateUserId)},
+            (error, response, body) => {
+                assert(body === 'public-id-7d9d163d366e5abdcfc48331a4bb9581b8f07b7f');
+                done();
+            });
     });
 });
---
+++ test/util.js
@@ -0,0 +1,3 @@
+/**
+ * Created by dmitry.turovtsov on 18.05.2017.
+ */
