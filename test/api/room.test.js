6c825a94c60f595b98440c2166baa0664d260d38
Dmitry Turovtsov <web.best.master@gmail.com>
2017-05-24 21:45:51 +0300

    update tests

--- test/api/room.test.js
+++ test/api/room.test.js
@@ -21,75 +21,77 @@ describe('api/room', () => {
     const privateSecondUserId = 'private-user-id-second-1234567890';
     // const publicSecondUserId = generatePublicId(privateUserId);
 
-    it('create room', () => {
-        return post(route.create).then(instanceId => {
-            firstRoomId = instanceId;
-            assert(/\d+/.test(instanceId));
-        });
+    it('create room', async () => {
+        firstRoomId = await post(route.create);
+        assert(/\d+/.test(firstRoomId));
     });
 
-    it('create room with parameters', () => {
-        return post(route.create, {key: 'value'})
-            .then(instanceId => {
-                secondRoomId = instanceId;
-                return get(route.getState.replace(':instanceId', secondRoomId).replace(':key', 'key'));
-            })
-            .then(result => assert(JSON.parse(result).result === 'value'));
+    it('create room with parameters', async () => {
+        secondRoomId = await post(route.create, {key: 'value'});
+
+        const state = await get(route.getState.replace(':instanceId', secondRoomId).replace(':key', 'key'));
+
+        assert(JSON.parse(state).result === 'value');
     });
 
-    it('get all rooms', () => {
-        return get(route.getItems)
-            .then(roomsId => {
-                assert(JSON.parse(roomsId).indexOf(firstRoomId) !== -1);
-                assert(JSON.parse(roomsId).indexOf(secondRoomId) !== -1);
-            });
+    it('get all rooms', async () => {
+        const roomsId = JSON.parse(await get(route.getItems));
+
+        assert(roomsId.indexOf(firstRoomId) !== -1);
+        assert(roomsId.indexOf(secondRoomId) !== -1);
     });
 
-    it('set/get room state(s)', () => {
-        return post(route.setState.replace(':instanceId', firstRoomId), {
+    it('set/get room state(s)', async () => {
+        await post(route.setState.replace(':instanceId', firstRoomId), {
             key1: 'value1',
             key2: 'value2'
-        })
-            .then(() => Promise.all([
-                get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'key1')),
-                get(route.getStates.replace(':instanceId', firstRoomId).replace(':keys', 'key1,key2'))
-            ]))
-            .then(([result, results]) => {
-                assert(JSON.parse(result).result === 'value1');
-                assert.deepEqual(JSON.parse(results).result, {key1: 'value1', key2: 'value2'});
-            });
+        });
+
+        const [result, results] = await Promise.all([
+            get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'key1')),
+            get(route.getStates.replace(':instanceId', firstRoomId).replace(':keys', 'key1,key2'))
+        ]);
+
+        assert(JSON.parse(result).result === 'value1');
+        assert.deepEqual(JSON.parse(results).result, {key1: 'value1', key2: 'value2'});
     });
 
-    it('push to key', () => {
-        return post(route.setState.replace(':instanceId', firstRoomId), {arr: ['firstValue']})
-            .then(() => post(route.pushToKey.replace(':instanceId', firstRoomId), {arr: 'secondValue'}))
-            .then(() => get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'arr')))
-            .then(result => assert.deepEqual(JSON.parse(result).result, ['firstValue', 'secondValue']));
+    it('push to key', async () => {
+        await post(route.setState.replace(':instanceId', firstRoomId), {arr: ['firstValue']});
+        await post(route.pushToKey.replace(':instanceId', firstRoomId), {arr: 'secondValue'});
+        const result = await get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'arr'));
+
+        assert.deepEqual(JSON.parse(result).result, ['firstValue', 'secondValue']);
     });
 
-    it('join', () => {
-        return get(route.join.replace(':instanceId', firstRoomId).replace(':privateUserId', privateUserId))
-            .then(() => get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'users')))
-            .then(result => assert.deepEqual(JSON.parse(result).result, [{publicId: publicUserId}]));
+    it('join', async () => {
+        await get(route.join.replace(':instanceId', firstRoomId).replace(':privateUserId', privateUserId));
+        const result = await get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'users'));
+
+        assert.deepEqual(JSON.parse(result).result, [{publicId: publicUserId}]);
     });
 
-    it('set user state', () => {
-        return post(route.setUserState.replace(':instanceId', firstRoomId).replace(':privateUserId', privateUserId),
-            {userKey: 'userValue'})
-            .then(() => get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'users')))
-            .then(result => assert.deepEqual(JSON.parse(result).result, [{
-                publicId: publicUserId,
-                userKey: 'userValue'
-            }]));
+    it('set user state', async () => {
+        await post(
+            route.setUserState.replace(':instanceId', firstRoomId).replace(':privateUserId', privateUserId),
+            {userKey: 'userValue'}
+        );
+        const result = await get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'users'));
+
+        assert.deepEqual(JSON.parse(result).result, [{
+            publicId: publicUserId,
+            userKey: 'userValue'
+        }]);
     });
 
-    it('leave', () => {
-        return get(route.leave.replace(':instanceId', firstRoomId).replace(':privateUserId', privateUserId))
-            .then(() => get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'users')))
-            .then(result => assert.property(JSON.parse(result), 'error'));
+    it('leave', async () => {
+        await get(route.leave.replace(':instanceId', firstRoomId).replace(':privateUserId', privateUserId));
+        const result = await get(route.getState.replace(':instanceId', firstRoomId).replace(':key', 'users'));
+
+        assert.property(JSON.parse(result), 'error');
     });
 
-    it('ping user', function withTimeOut(done) {
+    it.skip('ping user', function withTimeOut(done) {
         const {leaveUserTimeout} = roomProps;
         let testRoomId = '';
 
@@ -205,7 +207,7 @@ describe('api/room', () => {
                 assert(JSON.parse(result).result === 0));
     });
 
-    it('leave turn by disconnect', function withTimeOut(done) {
+    it.skip('leave turn by disconnect', function withTimeOut(done) {
         let roomId = '';
         const {leaveUserTimeout} = roomProps;
 
